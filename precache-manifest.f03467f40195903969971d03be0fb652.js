7dad4af8c1543b88dbe61c90a562b1b5c6800d4e
Takyiu Lo <takyiulo@gmail.com>
2018-10-03 20:49:49 -0400

    Updates

---
+++ precache-manifest.f03467f40195903969971d03be0fb652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "180f923038051d0b454f",
+    "url": "/calculator-react/static/js/runtime~main.180f9230.js"
+  },
+  {
+    "revision": "7e66aaf0967ed1423dd9",
+    "url": "/calculator-react/static/js/main.7e66aaf0.chunk.js"
+  },
+  {
+    "revision": "cb5e426cd31d723bb95e",
+    "url": "/calculator-react/static/js/1.cb5e426c.chunk.js"
+  },
+  {
+    "revision": "7e66aaf0967ed1423dd9",
+    "url": "/calculator-react/static/css/main.954620a5.chunk.css"
+  },
+  {
+    "revision": "8f74d4a3e377af491d128a43c83b740e",
+    "url": "/calculator-react/index.html"
+  }
+];
